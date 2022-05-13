8647cdb5c5d3bcdd726ab217a7399f0b30af327b
Marian Wang <marian.mian.wang@gmail.com>
2022-05-13 14:08:09 -0400

    added / path

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
+  console.log("Path:  ", request.path); 
   console.log("Body:  ", request.body);
   console.log("---");
   next();
