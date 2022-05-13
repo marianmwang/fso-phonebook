af076398b75a09c5f563e2f806b8fff358c6d45b
Marian Wang <marian.mian.wang@gmail.com>
2022-05-13 13:55:11 -0400

    add to heroku app

---
+++ index.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const morgan = require("morgan");
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(requestLogger);
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  const today = new Date().toString();
+  response.send(
+    `<p>Phonebook has entries for ${persons.length} people.</p>
+    <p>Request made at ${today}`
+  );
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const personId = Number(request.params.id);
+  const person = persons.find((x) => x.id === personId);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const personId = Number(request.params.id);
+  persons = persons.filter((x) => x.id !== personId);
+
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  const found = persons.find((x) => x.name === body.name);
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  } else if (!body.number) {
+    return response.status(400).json({
+      error: "phone number missing",
+    });
+  } else if (found) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 10000),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`part 3 exercises server running on port ${PORT}`);
+});
