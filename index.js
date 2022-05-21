055a3aea86b36dea8c6d1e7c2f6eedf918134eb7
Marian Wang <marian.mian.wang@gmail.com>
2022-05-21 13:42:53 -0400

    uibuild

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
 });
 const cors = require("cors");
+const Person = require("./models/person");
 
 const app = express();
-
-app.use(cors());
-app.use(express.static("build"));
-app.use(express.json());
-app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -22,97 +17,85 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 app.use(requestLogger);
-
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 app.get("/", (req, res) => {
   res.status(404).end();
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/info", (request, response) => {
   const today = new Date().toString();
-  response.send(
-    `<p>Phonebook has entries for ${persons.length} people.</p>
-    <p>Request made at ${today}`
-  );
+  Person.find({}).then((persons) => {
+    response.send(
+      `<p>Phonebook has entries for ${persons.length} people.</p>
+      <p>Request made at ${today}`
+    );
+  });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const personId = Number(request.params.id);
-  const person = persons.find((x) => x.id === personId);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const personId = Number(request.params.id);
-  persons = persons.filter((x) => x.id !== personId);
-
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((res) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
-  const found = persons.find((x) => x.name === body.name);
-  if (!body.name) {
-    return response.status(400).json({
-      error: "name missing",
-    });
-  } else if (!body.number) {
-    return response.status(400).json({
-      error: "phone number missing",
-    });
-  } else if (found) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
     id: Math.floor(Math.random() * 10000),
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(person);
+  });
 });
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
+  response.status(404).send({ error: "Unknown endpoint" });
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "Malformatted ID" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
