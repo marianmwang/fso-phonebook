c5d6d25f495419e06310241f3f620d153735b08b
Marian Wang <marian.mian.wang@gmail.com>
2022-05-21 22:12:23 -0400

    uibuild

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const requestLogger = (request, response, next) => {
 app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
-app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
+// app.use(
+//   morgan(":method :url :status :res[content-length] - :response-time ms :body")
+// );
 app.use(requestLogger);
 
 app.get("/", (req, res) => {
@@ -59,24 +59,45 @@ app.get("/api/persons/:id", (request, response, next) => {
 
 app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-    .then((res) => {
-      res.status(204).end();
+    .then((result) => {
+      response.status(204).end();
     })
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   const person = new Person({
     name: body.name,
     number: body.number,
-    id: Math.floor(Math.random() * 10000),
   });
 
-  person.save().then((savedPerson) => {
-    response.json(person);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
@@ -90,6 +111,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "Malformatted ID" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
--- models/person.js
+++ models/person.js
@@ -14,13 +14,25 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function(value) {
+        return (/\d{2}-\d{6,}/.test(value) || /\d{3}-\d{5,}/.test(value)) || /\d{8,}/.test(value)
+      }
+    },
+    required: true,
+  },
 });
 
 personSchema.set("toJSON", {
   transform: (doc, returnedObject) => {
-    returnedObject.id = returnedObject.id.toString();
+    returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
   },
